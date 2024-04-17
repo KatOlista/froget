35dee172311e25382837fa2a2ef09efe22d414ac
KatOlista^^! <olya.katyushina@gmail.com>
2024-04-17 19:46:22 +0200

    fix circle animation

--- src/components/UI/Circle/Circle.jsx
+++ src/components/UI/Circle/Circle.jsx
@@ -2,12 +2,19 @@ import styles from './Circle.module.scss';
 
 export const Circle = () => {
   return (
-    <div className={styles.circle}>
-      <div className={styles.circle__circle}>
-        <div className={styles.circle__number}>%</div>
+    <div>
+      <div className={styles.circle}>
+        <div className={styles.number}>%</div>
       </div>
 
-      <div className={styles.circle__filler}>
+      <div className={styles.container}>
+        <div className={styles.box}>
+          <div className={styles.innerBox}>
+            <div className={styles.ellipse}></div>
+
+            <div className={styles.ellipse2}></div>
+          </div>
+        </div>
       </div>
     </div>
   );
