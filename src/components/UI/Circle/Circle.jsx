93c917d89fb17cfc9956bad899c253d2787cf4fb
KatOlista^^! <olya.katyushina@gmail.com>
2024-04-27 23:24:29 +0200

    fix count from 0 to 100 animation

--- src/components/UI/Circle/Circle.jsx
+++ src/components/UI/Circle/Circle.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+
+import { useInterval } from '../../../hooks';
+
 import styles from './Circle.module.scss';
 
 export const Circle = () => {
+  let [count, setCount] = useState(0);
+
+  const time = 4;
+  const maxCount = 100;
+
+  const delay = 1000 * time / maxCount;
+
+  useInterval(() => {
+    if (count < maxCount) {
+      setCount(count + 1);
+    }
+  }, delay);
+
   return (
     <div>
       <div className={styles.circle}>
-        <div id='number' className={styles.number}>%</div>
+        {/* <div id='number' className={styles.number}>%</div> */}
+        <div className={styles.number}>{count}%</div>
       </div>
 
       <div className={styles.container}>
